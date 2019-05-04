6320841efab00e0400266dd7d44b54902e24824f
BlackFire-Desktop <GMate375@gmail.com>
2019-05-04 19:37:11 +0200

    Trials (and errors...)

--- client.py
+++ client.py
@@ -1,10 +1,28 @@
-from base64 import b64encode
+# System imports
+import sys, socket, base64
+
+# PyCryptodome imports
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 
-header = b"header"
-data = b"secret"
+def encryptMessage(key, header, nonce, data):
+    cipher = AES.new(key, AES.MODE_GCM, nonce)
+    cipher.update(header)
+    ciphertext, tag = cipher.encrypt_and_digest(data)
+    return ciphertext, tag
+
+def decryptMessage(key, header, nonce, tag, data):
+    cipher = AES.new(key, AES.MODE_CCM, nonce)
+    cipher.update(header)
+    return cipher.decrypt_and_verify(data, tag)
+
+
 key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_GCM)
-cipher.update(header)
-ciphertext, tag = cipher.encrypt_and_digest(data)
+header = b"header"
+nonce = b"0123456789"
+
+secretMessage, tag = encryptMessage(key, header, nonce, b"My secret message.")
+decryptedMessage = decryptMessage(key, header, nonce, tag, secretMessage)
+
+print("The message was: " + decryptedMessage)
