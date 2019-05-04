227ec2d7611a6c54af612718e380a16a874f3ef1
BlackFire-Desktop <GMate375@gmail.com>
2019-05-04 18:38:01 +0200

    Added PyCryptodome

--- client.py
+++ client.py
@@ -1,1 +1,10 @@
-print("Hello World") # comment
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+header = b"header"
+data = b"secret"
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_GCM)
+cipher.update(header)
+ciphertext, tag = cipher.encrypt_and_digest(data)
